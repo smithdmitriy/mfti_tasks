bcc6bcd4b1dabad0036fdd38b24947511e85e6bd
smithdmitriy <smithdmitriy@gmail.com>
2021-11-12 18:58:22 +0500

    add example

---
+++ example_5.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-10, 10.01, 0.01)
+t = np.arange(-10, 11, 1)
+plt.figure(figsize=(18, 9))
+
+#subplot 1
+sp = plt.subplot(221)
+plt.plot(x, np.sin(x))
+plt.title(r'$\sin(x)$')
+plt.grid(True)
+
+#subplot 2
+sp = plt.subplot(222)
+plt.plot(x, np.cos(x), 'g')
+plt.axis('equal')
+plt.grid(True)
+plt.title(r'$\cos(x)$')
+
+     #subplot 3
+sp = plt.subplot(223)
+plt.plot(x, x**2, t, t**2, 'ro')
+plt.title(r'$x^2$')
+
+#subplot 4
+sp = plt.subplot(224)
+plt.plot(x, x)
+sp.spines['left'].set_position('center')
+sp.spines['bottom'].set_position('center')
+plt.title(r'$x$')
+
+plt.savefig('4_figure_with_legend.png')
+
+plt.show()
