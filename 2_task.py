6b172c1b51ccca66441de3751a1b347b2c42f0d3
smithdmitriy <smithdmitriy@gmail.com>
2021-11-13 11:11:57 +0500

    add minor ticks on grid

--- 2_task.py
+++ 2_task.py
@@ -1,7 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-5, 5, 0.001)
-plt.plot(x, x**2 - x - 6)
-plt.grid(True)
+x = np.arange(-3, 4, 0.001)
+plt.plot(x, x ** 2 - x - 6)
+plt.minorticks_on()
+plt.grid(which='major',
+         color='k',
+         linewidth=1)
+plt.grid(which='minor',
+         color='k',
+         linestyle=':')
 plt.show()
