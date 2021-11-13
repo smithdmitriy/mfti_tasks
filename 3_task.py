2251d4c6ac4760a429e1fecb91fbaa5567da4288
smithdmitriy <smithdmitriy@gmail.com>
2021-11-13 15:06:55 +0500

    add title of foo_3

--- 3_task.py
+++ 3_task.py
@@ -14,7 +14,9 @@ plt.figure(figsize=(14, 7))
 plt.plot(x, foo_3(x), lw=0.5)
 plt.xlabel(r'$x$', fontsize=14)
 plt.ylabel(r'$f(x)$', fontsize=14)
-plt.title(r'$\sin(x)$') #TODO write title jf the foo_3
+plt.title(r'$f\left(x \right)=\log _{1+\tan \left(\frac{1}{1+\sin ^{2}\left(x '
+          r'\right)} \right)}\left(x^{2}+1\right)\exp \left(-\frac{\left|x '
+          r'\right|}{10} \right)$',fontsize=20)
 plt.minorticks_on()
 plt.grid(which='major',
          color='k',
