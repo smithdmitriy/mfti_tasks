6d6a91e8e4685c748ae3dc827ff2ac2a7103e2b7
smithdmitriy <smithdmitriy@gmail.com>
2021-11-16 08:22:18 +0500

    add examples

--- examples/example_1.py
+++ examples/example_1.py
@@ -4,3 +4,4 @@ import matplotlib.pyplot as plt
 x = np.arange(-10, 10.01, 0.01)
 plt.plot(x, x**2)
 plt.show()
+
--- examples/example_5.py
+++ examples/example_5.py
@@ -1,28 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 x = np.arange(-10, 10.01, 0.01)
 t = np.arange(-10, 11, 1)
 plt.figure(figsize=(18, 9))
 
-#subplot 1
+# subplot 1
 sp = plt.subplot(221)
 plt.plot(x, np.sin(x))
 plt.title(r'$\sin(x)$')
 plt.grid(True)
 
-#subplot 2
+# subplot 2
 sp = plt.subplot(222)
 plt.plot(x, np.cos(x), 'g')
 plt.axis('equal')
 plt.grid(True)
 plt.title(r'$\cos(x)$')
 
-     #subplot 3
+# subplot 3
 sp = plt.subplot(223)
-plt.plot(x, x**2, t, t**2, 'ro')
+plt.plot(x, x ** 2, t, t ** 2, 'ro')
 plt.title(r'$x^2$')
 
-#subplot 4
+# subplot 4
 sp = plt.subplot(224)
 plt.plot(x, x)
 sp.spines['left'].set_position('center')
